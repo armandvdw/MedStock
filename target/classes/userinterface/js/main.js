26ec0189e9c5feeecdae5941dc429a799709112a
armand <armandvanderwesthuizen02@gmail.com>
2014-10-07 11:07:02 +0200

    Slick Grid Added

--- target/classes/userinterface/js/main.js
+++ target/classes/userinterface/js/main.js
@@ -5,19 +5,58 @@ function prepareMap() {
 }
 
 function prepareGrid() {
-    //Get a slickgrid going here
+    var grid;
+    var columns = [
+        {id: "ID", name: "ID", field: "id",maxWidth:30,width:25},
+        {id: "Name", name: "Name", field: "name"},
+        {id: "Country", name: "Country", field: "country"},
+        {id: "Nevirapine", name: "Nevirapine", field: "nev"},
+        {id: "Stavudine", name: "Stavudine", field: "sta"},
+        {id: "Zidotabine", name: "Zidotabine", field: "zid"}
+    ];
+
+    var options = {
+        enableCellNavigation: true,
+        enableColumnReorder: false,
+        forceFitColumns: true
+
+    };
+
+
+    var data = [];
+    for (var i = 0; i < 10; i++) {
+        data[i] = {
+            id: + i,
+            name: "days",
+            country: "ZA",
+            nev: Math.round(Math.random() * 100),
+            sta: Math.round(Math.random() * 100),
+            zid: Math.round(Math.random() * 100)
+        };
+    }
+
+    grid = new Slick.Grid("#ms-propbox", data, columns, options);
 
 }
-//TODO: try to make a clinic object with attributes.
-//This is the entire
+//TODO: replace temp clinic with backend data.
+var tempClinic = {
+    clinName: "Mezza",
+    id: "001",
+    country: "ZA",
+    nev: "10",
+    sta: "5",
+    zid: "8"
+};
+
+
 function preparePropertyBox(divId, clinic) {
-    var propboxHeading = "<h2>Clinic: " + clinic.name + "</h2>";
+    var propboxHeading = "<h2>Clinic: " + clinic.clinName + "</h2>";
     var propboxFields = "<ul>" +
         "<li>Clinic ID: " + clinic.id + "</li>" +
         "<li>Country: " + clinic.country + "</li>" +
-        "<li>Nevirapine: " + clinic.nevirapineStock + "</li>" +
-        "<li>Stavudine: " + clinic.stavudineStock + "</li>" +
-        "<li>Zidotabine: " + clinic.zidotabine + "</li>" +
+        "<li>Nevirapine: " + clinic.nev + "</li>" +
+        "<li>Stavudine: " + clinic.sta + "</li>" +
+        "<li>Zidotabine: " + clinic.zid + "</li>" +
         "</ul>";
 
     $("#" + divId).append(propboxHeading);
@@ -33,18 +72,18 @@ function prepareChart(divId, clinicData) {
         series: {
             bars: {
                 show: true,
-                barWidth: 1/chartData.ticks.length,
+                barWidth: 1 / chartData.ticks.length,
                 order: 1,
-                align:"center"
+                align: "center"
             }
         },
         xaxis: {
-           //tickSize: 1,
+            //tickSize: 1,
             tickLength: 0,
             axisLabel: "Clinics",
             axisLabelUseCanvas: true,
             axisLabelFontFamily: "Verdana",
-            axisLabelFontSizePixels: 18,
+            axisLabelFontSizePixels: 15,
             axisLabelPadding: 20,
             mode: "categories",
             ticks: chartData.ticks
@@ -53,21 +92,22 @@ function prepareChart(divId, clinicData) {
             axisLabel: "MedicineStock",
             axisLabelUseCanvas: true,
             axisLabelFontFamily: "verdana",
-            axisLabelFontSizePixels: 20,
-            axisLabelPadding: 20
+            axisLabelFontSizePixels: 15,
+            axisLabelPadding: 18
         },
         selection: { mode: "x" }
     };
-    $.plot($("#" + divId),chartData.data,settings);
+    $.plot($("#" + divId), chartData.data, settings);
 }
 
 function loadContent() {
-  //  preparePropertyBox("ms-propbox");
+    //preparePropertyBox("ms-propbox", tempClinic);
     prepareChart("ms-grid", mockedBarGraphData);
+    prepareGrid();
 }
 //TODO: remove this after implementation!
 var mockedBarGraphData = [
-     {
+    {
         "name": "Mezza",
         "id": "001",
         "country": "ZA",
@@ -105,32 +145,32 @@ function barGraphDataPreparation(sourceData) {
 
     var chartData = {
         ticks: [],
-        data:[]
+        data: []
     };
     var nevirapine = [];
     var stavudine = [];
     var zidotabine = [];
-    for (var i = 0; i < sourceData.length;i++){
+    for (var i = 0; i < sourceData.length; i++) {
         var clinic = sourceData[i];
         var clinName = clinic["name"];
-        nevirapine.push([i,clinic["nev"]]);
-        stavudine.push([i,clinic["sta"]]);
-        zidotabine.push([i,clinic["zid"]]);
-        chartData.ticks.push([i,clinName]);
+        nevirapine.push([i, clinic["nev"]]);
+        stavudine.push([i, clinic["sta"]]);
+        zidotabine.push([i, clinic["zid"]]);
+        chartData.ticks.push([i, clinName]);
     }
-        chartData.data.push({
-            data: nevirapine,
-            label:"nevirapine"
-        });
-        chartData.data.push({
-            data:stavudine,
-            label:"stavudine"
-        });
+    chartData.data.push({
+        data: nevirapine,
+        label: "nevirapine"
+    });
+    chartData.data.push({
+        data: stavudine,
+        label: "stavudine"
+    });
 
-        chartData.data.push({
-            data:zidotabine,
-            label:"zidotabine"
-        });
+    chartData.data.push({
+        data: zidotabine,
+        label: "zidotabine"
+    });
 
     return chartData;
 }
