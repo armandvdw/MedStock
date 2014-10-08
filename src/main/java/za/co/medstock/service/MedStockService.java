6ee30cfdcbd3909bafb65d6c2ffe07da1f776510
armand <armandvanderwesthuizen02@gmail.com>
2014-10-08 17:52:46 +0200

    Added RESTful methods

--- src/main/java/za/co/medstock/service/MedStockService.java
+++ src/main/java/za/co/medstock/service/MedStockService.java
@@ -1,7 +1,8 @@
 package za.co.medstock.service;
 
 import com.google.gson.Gson;
-import org.hibernate.Session;
+import spark.Request;
+import spark.Route;
 import za.co.medstock.crud.HibernateUtil;
 import za.co.medstock.crud.MedStock;
 import za.co.medstock.entities.Clinic;
@@ -15,8 +16,10 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static spark.Spark.get;
-import static spark.SparkBase.*;
+import static spark.Spark.*;
+import static spark.SparkBase.setPort;
+import static spark.SparkBase.staticFileLocation;
+import static spark.SparkBase.stop;
 
 public class MedStockService {
 
@@ -72,11 +75,22 @@ public class MedStockService {
         settings.put(key, value);
     }
 
+    public Clinic mapRequestToClinic(Request request){
+        String name = request.queryParams("name");
+        String country = request.queryParams("country");
+        Integer nevirapine =  Integer.valueOf(request.queryParams("nev"));
+        Integer stavudine= Integer.valueOf(request.queryParams("sta"));
+        Integer zidotabine= Integer.valueOf(request.queryParams("zid"));
+        Double lat = Double.parseDouble(request.queryParams("lat"));
+        Double lon = Double.parseDouble(request.queryParams("lon"));
+        return new Clinic(1,name,country,nevirapine,stavudine,zidotabine,lat,lon);
+    }
     public static void main(String[] args) {
-        setPort(80);
-        HibernateUtil.createSessionFactory(HIBERNATE_CONFIG);
+        setPort(12345);
+        // HibernateUtil.createSessionFactory(HIBERNATE_CONFIG);
         MedStockService medServ = new MedStockService();
-        Session session = HibernateUtil.getCurrentSession();
+        Gson gson = new Gson();
+        //Session session = HibernateUtil.getCurrentSession();
         MedStock med = new MedStock();
 
         staticFileLocation("/userinterface");
@@ -84,7 +98,7 @@ public class MedStockService {
         get("/hello", (request, response) -> {
             return "Hello World!";
         });
-
+        //TODO: Remove these set methods.
         get("/", (request, response) -> {
             set("title", "MedStock Stock Management ");
             set("count", String.valueOf(settings.size()));
@@ -95,14 +109,56 @@ public class MedStockService {
             set("count", String.valueOf(settings.size()));
             return medServ.render("userinterface/main.html", settings);
         });
-        get("/clinicData", (request, response) -> {
-            ArrayList<Clinic> list = med.getAllClinics();
+        get("/clinics", (request, response) -> {
+            response.status(200);
             return medServ.convertToJSON(med.getAllClinics());
         });
         get("/logout", (request, response) -> {
             HibernateUtil.closeSession();
             stop();
             return "";
+        });
+        // Creates a new book resource, will return the ID to the created resource
+        // author and title are sent as query parameters e.g. /books?author=Foo&title=Bar
+        post("/clinic", (request, response) -> {
+            Clinic clin = medServ.mapRequestToClinic(request);
+            if (clin !=null){
+                med.addNewEntity(clin);
+                response.status(201); // 201 Created
+                return true;
+            }
+
+            response.status(500); // Server Error
+            return false;
+        });
+
+        // Gets the book resource for the provided id
+        get("/clinic/:id", (request, response) -> {
+            Clinic clin = med.getClinic(request.params(":id"));
+            if (clin != null) {
+                return gson.toJson(clin);
+            } else {
+                response.status(404); // 404 Not found
+                return "Clinic has not been found";
+            }
+        });
+
+        // Updates the book resource for the provided id with new information
+        // author and title are sent as query parameters e.g. /books/<id>?author=Foo&title=Bar
+        put("/clinic/:id", (Route) (request, response) -> {
+            String id = request.params(":id");
+            //TODO: this step can be removed depending what gets returned by hibernate after entity update ex. returns 1, updated records =  1
+            Clinic serverClinic = med.getClinic(id);
+            if (serverClinic != null) {
+                Clinic updatedClinic = medServ.mapRequestToClinic(request);
+                updatedClinic.setClinicId(Integer.parseInt(id));
+                med.updateEntity(updatedClinic);
+                response.status(200);
+                return "Clinic: " + updatedClinic.getName() + "updated successfully";
+            } else {
+                response.status(404); // 404 Not found
+                return "Clinic does not exist";
+            }
         });
     }
 }
