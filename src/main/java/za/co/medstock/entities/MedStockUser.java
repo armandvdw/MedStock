5d72cc77f56ba14a70db237bf38157d12bbf67c8
armand <armandvanderwesthuizen02@gmail.com>
2014-10-15 07:37:07 +0200

    Updates made to new entities

--- src/main/java/za/co/medstock/entities/MedStockUser.java
+++ src/main/java/za/co/medstock/entities/MedStockUser.java
@@ -6,15 +6,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class User {
+public class MedStockUser implements Serializable {
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     Integer userId;
     String userName;
     String userPassword;
