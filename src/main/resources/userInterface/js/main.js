85163ec54ae4946a2be6e6c5cf3090378ebd9322
armand <armandvanderwesthuizen02@gmail.com>
2014-10-15 22:18:25 +0200

    Map page close to finished

--- src/main/resources/userInterface/js/main.js
+++ src/main/resources/userInterface/js/main.js
@@ -1,5 +1,5 @@
 //Some Global variables needed to control some state of components
-var clinicData =[undefined]; //JSON.parse(getClinicData());
+var clinicData = [undefined]; //JSON.parse(getClinicData());
 var mapZoomLevel = 3;//This is the default setting for the leaflet map
 var mapLat = 0.56;
 var mapLon = 22.89;
@@ -9,18 +9,18 @@ var displayedWarning = false;
 
 //This is used to load the Components with data/ also used as a refresher.
 function loadContent() {
- /*   if (!clinicData) {
-        alert("No data Received");
-    }
-	if(!displayedWarning){
-		displayLowStockWarning(clinicData);
-	}
-    prepareChart("ms-chart", clinicData);
-    prepareGrid("ms-grid", clinicData);
-    prepareMap("ms-map", clinicData);*/
+    /*   if (!clinicData) {
+     alert("No data Received");
+     }
+     if(!displayedWarning){
+     displayLowStockWarning(clinicData);
+     }
+     prepareChart("ms-chart", clinicData);
+     prepareGrid("ms-grid", clinicData);
+     prepareMap("ms-map", clinicData);*/
 }
 
-function loadChart(){
+function loadChart() {
 
     var data = JSON.parse(getClinicData());
     if (!data) {
@@ -28,32 +28,32 @@ function loadChart(){
     }
     prepareChart("ms-chart", data);
 }
-function loadMap(){
+function loadMap() {
     var data = JSON.parse(getClinicData());
     if (!data) {
         alert("No data Received");
     }
-    prepareMap("ms-map",data);
+    prepareMap("ms-map", data);
 }
-function loadGrid(){
+function loadGrid() {
     var data = JSON.parse(getClinicData());
     if (!data) {
         alert("No data Received");
     }
-    prepareGrid("ms-grid",data);
+    prepareGrid("ms-grid", data);
 }
 
 //Set the global content to all stock
 function setContentToAllStock() {
     clinicData = JSON.parse(getClinicData());
-	displayLowStockWarning(clinicData);
+    displayLowStockWarning(clinicData);
     loadContent();
 }
 
 //Gets the low stock and updates the global content to show it.
 function setContentToLowStock() {
     clinicData = JSON.parse(getLowStockClinics());
-	displayLowStockWarning(clinicData);
+    displayLowStockWarning(clinicData);
     loadContent();
 }
 //TODO: check if this works
@@ -64,12 +64,9 @@ function lowStockClinic(clinic) {
 
 //This will do the setup for my map component
 function prepareMap(divId, cd) {
-    //Basic map initializer
-    var map = L.map(divId, {
-        closePopupOnClick: true
-    });
 
-    map.setView([mapLat, mapLon], mapZoomLevel);
+    var map = L.map(divId).setView([-3.0, 23.0],3);
+
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -78,111 +75,32 @@ function prepareMap(divId, cd) {
         id: 'examples.map-i875mjb7'
     }).addTo(map);
 
-    //Add amrker layer to the map to enable marker shortcuts
-    var markersLayer = new L.LayerGroup();	//layer contain searched elements
-    map.addLayer(markersLayer);
-
-    //Create the map content
     for (var i = 0; i < cd.length; i++) {
         var clinic = cd[i];
+        //TODO: implement this by setting the anchor  also add tooltip to marker by setting the title
 
-        //Create the update button
-        var buttonUpdate = $("<button id='btn-update' type=submit>Update</button>").click(function (data) {
-            var form = $("#form-update");
-            updateClinic(form.serialize());
+/*        var icon = L.icon({
+            iconUrl: "images/marker-icon-green.png",
+            shadowUrl: 'images/marker-shadow.png'
+
         });
+        if (lowStockClinic(clinic) === false){
+            icon = L.icon({
+                iconUrl: "images/marker-icon-red.png",
+                shadowUrl: 'images/marker-shadow.png'
+            });
+        }*/
 
-        //Create the Delete button
-        var buttonDelete = $("<button id='btn-delete' type=submit>Delete</button>").click(function (data) {
-            var form = $("#form-update");
-            if (confirm("Are you sure you want to delete this clinic?"))deleteClinic(form.serializeObject()["clinicId"]);
-        });
+        var popupText = "<h4>Name: " + clinic["name"] + " </h4>" +
+            "Nevirapine: " + clinic["nevirapineStock"] + "<br/>" +
+            "Stavudine: " + clinic["stavudineStock"] + "<br/> " +
+            "Zidotabine: " + clinic["zidotabineStock"];
 
-        //Create the cancel button.
-        var buttonCancel = $("<button id='btn-cancel'>Cancel</button>").click(function (data) {
-            map.closePopup()
-        });
-
-        //A reusable function to create fields.
-        var field = function (name, label, value) {
-            return "<div style='padding: 2px 0 2px 0'>" +
-                "<label style='width: 120px;display: inline-block;'>" + label + "</label>" +
-                "<input name=" + name + " type='text' style='width: 204px;' value='" + value + "'/>" +
-                "</div>";
-        };
-
-        //Create the form to be displayed in the popup
-        var formUpdate = $("<form id='form-update'>");
-        formUpdate.append("<h1>Clinic: " + clinic.name + "</h1>");
-        // form.append("<form id='update-form'>");
-        formUpdate.append(field("clinicId", "Clinic ID:", clinic.clinicId));
-        formUpdate.append(field("name", "Name:", clinic.name));
-        formUpdate.append(field("countryName", "Country:", clinic.countryName));
-        formUpdate.append(field("nevirapineStock", "Nevirapine:", clinic.nevirapineStock));
-        formUpdate.append(field("stavudineStock", "Stavudine:", clinic.stavudineStock));
-        formUpdate.append(field("zidotabineStock", "Zidotabine:", clinic.zidotabineStock));
-        formUpdate.append(field("latitude", "Latitude:", clinic.latitude));
-        formUpdate.append(field("longitude", "Longitude:", clinic.longitude));
-        formUpdate.append("</form>");
-        formUpdate.append(buttonUpdate);
-        formUpdate.append(buttonDelete);
-        formUpdate.append(buttonCancel);
-
-        //Add red markers to the map where stock is low
-        var marker = L.marker([clinic.latitude, clinic.longitude], {title: clinic["name"]});
-        if (lowStockClinic(clinic)) {
-            marker.setIcon = L.Icon({ iconUrl: 'js/images/marker-icon-red.png'});
-        }
-        marker.addTo(markersLayer);
-        marker.bindPopup(formUpdate[0]);
+        L.marker([clinic["latitude"], clinic["longitude"]])
+           // .setIcon(icon)
+            .addTo(map)
+            .bindPopup(popupText);
     }
-    //
-    var formCreate = function (event) {
-
-        var buttonCreate = $("<button id='btn-create' type=submit>Create</button>").click(function () {
-            var form = $("#form-create");
-            createClinic(form.serialize());
-        });
-        var formCreate = $("<form id='form-create'>");
-        formCreate.append("<h1>Add Clinic</h1>");
-        // form.append("<form id='update-form'>");
-        formCreate.append(field("name", "Name:", ""));
-        formCreate.append(field("countryName", "Country:", ""));
-        formCreate.append(field("nevirapineStock", "Nevirapine:", ""));
-        formCreate.append(field("stavudineStock", "Stavudine:", ""));
-        formCreate.append(field("zidotabineStock", "Zidotabine:", ""));
-        formCreate.append(field("latitude", "Latitude:", event.latlng.lat));
-        formCreate.append(field("longitude", "Longitude:", event.latlng.lng));
-        formCreate.append("</form>");
-        formCreate.append(buttonCreate);
-        return formCreate;
-    };
-    //Create the popup to add the new clinic
-    var popup = L.popup();
-    function onMapClick(e) {
-        if (createMode) {
-            popup
-                .setLatLng(e.latlng)
-                .setContent(formCreate(e)[0])
-                .openOn(map);
-        }
-    }
-
-    //Create the markers list to add to the map
-    var list = new L.Control.ListMarkers({layer: markersLayer, itemIcon: null});
-    //TODO: see if you can fix this
-    /*  list.on('item-mouseover',function (e) {
-     e.layer.options.icon.options.iconUrl = L.Icon.Default.imagePath + 'marker-icon-green.png';
-     (L.icon({
-     iconUrl: L.Icon.Default.imagePath + 'marker-icon-green.png'
-     })                    );
-     }).on('item-mouseout', function (e) {
-     e.layer.setIcon(L.icon({
-     iconUrl: L.Icon.Default.imagePath+'/marker-icon.png'
-     }))
-     });  */
-    map.addControl(list);
-    map.on('click', onMapClick);
 }
 
 //This will toggle the stat of the button to create new clinics on map click
@@ -220,10 +138,6 @@ function prepareGrid(divId, cd) {
         selectedClinic = (grid.getData()[grid.getSelectedRows()]);
         setMapSelectedMarker(selectedClinic["clinicId"]);
     });
-}
-//TODO: this does not work yet
-function setMapSelectedMarker(id) {
-    //IF there is a way to click on the map, this would be the spot to add it to integrat grid and map
 }
 
 //This is the chart setup
@@ -361,30 +275,30 @@ $.fn.serializeObject = function () {
 };
 //This will display the warning for low stock clinics
 
-function displayLowStockWarning(clinics){
-	var alertOutput = "Warning! The Following Clinics are low on stock: \n";
-	for(var i in clinics){
-		var nev = clinics[i]["nevirapineStock"];
-		var sta = clinics[i]["stavudineStock"];
-		var zid = clinics[i]["zidotabineStock"];
-		var clinName = clinics[i]["name"];
-		var cls = "\t•"+clinName+ " is low on:";
+function displayLowStockWarning(clinics) {
+    var alertOutput = "Warning! The Following Clinics are low on stock: \n";
+    for (var i in clinics) {
+        var nev = clinics[i]["nevirapineStock"];
+        var sta = clinics[i]["stavudineStock"];
+        var zid = clinics[i]["zidotabineStock"];
+        var clinName = clinics[i]["name"];
+        var cls = "\t•" + clinName + " is low on:";
 
-		if(nev < 5){
-			cls+= " Nevirapine;";
-		}else if(sta < 5){
-			cls+= " Stavudine;"
-		}else if(zid < 5){
-			cls+= " Zidotabine"
-		}else{
-			alertOutput+="";
-			continue;
-		}
-		cls+="\n";
-		alertOutput += cls;
-	}
-	//alert(alertOutput);
-	displayedWarning = true;
+        if (nev < 5) {
+            cls += " Nevirapine;";
+        } else if (sta < 5) {
+            cls += " Stavudine;"
+        } else if (zid < 5) {
+            cls += " Zidotabine"
+        } else {
+            alertOutput += "";
+            continue;
+        }
+        cls += "\n";
+        alertOutput += cls;
+    }
+    //alert(alertOutput);
+    displayedWarning = true;
 }
 
 
